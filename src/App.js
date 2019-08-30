5ce6ee4fb3a9be88eb988973589daa73dca28ae6
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-30 18:39:48 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Content from './components/Content';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar></Sidebar>
+      <Content></Content>
+    </div>
+  );
+}
+
+export default App;
