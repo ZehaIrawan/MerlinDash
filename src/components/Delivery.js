eae8b14c0c2d50efcbb4ffa83f646bdb14487063
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-31 18:38:31 +0700

    mvp done

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Chart from './Chart';
 import Sidebar from './Sidebar';
 
 const Delivery = () => {
+  const input = {
+    labels: [
+      '12 am',
+      '2 am',
+      '4 am',
+      '6 am',
+      '8 am',
+      '10 am',
+      '12 pm',
+      '14 pm',
+      '16 pm',
+      '18 pm',
+      '20 pm',
+      '22 pm',
+    ],
+    datasets: [
+      {
+        label: 'Population',
+        data: [0, 200, 250, 300, 250, 200, 200, 400, 200, 250, 250, 200, 200],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+          'rgba(255, 99, 132, 0.6)',
+        ],
+      },
+    ],
+  };
+  const [data, setData] = useState(input);
   return (
     <div className="container">
       <Sidebar></Sidebar>
@@ -21,6 +54,9 @@ const Delivery = () => {
           <div className="card2"></div>
           <div className="card3"></div>
           <div className="card4"></div>
+        </div>
+        <div className="chart">
+          <Chart data={data}></Chart>
         </div>
       </div>
     </div>
